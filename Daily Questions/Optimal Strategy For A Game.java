8d7acfc4bd5713dd193662a8e9d973f5d2ebaf03
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-04-08 21:35:32 +0530

    Optimal Strategy For A Game
    This code implements a dynamic programming solution to find the maximum possible amount of money that can be won in a game where players can pick from either end of an array of coins.
    
    The function countMaximum takes two arguments: n, the size of the array, and arr[], the array containing values of coins.
    
    It initializes a 2D array table[][] to store the maximum amounts of money that can be won for subarrays of different lengths.
    
    Then, it iterates through all possible subarray lengths (gap) and for each length, it iterates through all subarrays of that length, calculating the maximum amount of money that can be won.
    
    Within the loop, it considers three possible scenarios for picking coins: either picking x coins from the i+2 position, y coins from both ends, or z coins from the j-2 position. It selects the scenario that results in the maximum amount of money and updates the table[][] accordingly.
    
    Finally, it returns the maximum amount of money that can be won from the entire array, which is stored in table[0][n-1].

---
+++ Daily Questions/Optimal Strategy For A Game.java
@@ -0,0 +1,28 @@
+class solve
+{
+    //Function to find the maximum possible amount of money we can win.
+    static long countMaximum(int n, int arr[])
+    {
+        // Your code here
+        int table[][] = new int[n][n];
+        int gap, i, j, x, y, z;
+ 
+        for (gap = 0; gap < n; ++gap) {
+            for (i = 0, j = gap; j < n; ++i, ++j) {
+ 
+                
+                x = ((i + 2) <= j) ? table[i + 2][j] : 0;
+                y = ((i + 1) <= (j - 1))
+                        ? table[i + 1][j - 1]
+                        : 0;
+                z = (i <= (j - 2)) ? table[i][j - 2] : 0;
+ 
+                table[i][j]
+                    = Math.max(arr[i] + Math.min(x, y),
+                               arr[j] + Math.min(y, z));
+            }
+        }
+ 
+        return table[0][n - 1];
+    }
+}
