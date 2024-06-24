5032a2202dc0bbb160ec3d578a521523c2569c9d
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-06-24 17:32:09 +0530

    Kadane's Algorithm

---
+++ Problem Set/KadaneAlgorithm.java
@@ -0,0 +1,20 @@
+class Solution{
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        long sum = 0;
+        long max = Long.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum>max){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
+
