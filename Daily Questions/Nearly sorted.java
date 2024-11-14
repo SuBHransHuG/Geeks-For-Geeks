e7c35c503f177e5b4821d01858fd5a0063cbfe9e
SUBHRANSHU GHADAI <69098939+SuBHransHuG@users.noreply.github.com>
2024-11-14 23:06:25 +0530

    Nearly sorted

---
+++ Daily Questions/Nearly sorted.java
@@ -0,0 +1,7 @@
+class Solution {
+    // Non-static method, so you need to call it on an instance of the class
+    public void nearlySorted(int[] arr, int k) {
+        // code
+        Arrays.sort(arr);
+    }
+}
